d19da1ea525643ab24c201d76ec7d262adace9b2
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-03-23 15:23:03 +0000

    authorization is now working

---
+++ auth.py
@@ -0,0 +1,20 @@
+import bcrypt 
+
+def check_credentials(login, password, db):
+    print("check credentials")
+    
+    password = password.encode('utf8')
+    
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM users WHERE username=%s;", (login,))
+        record = cursor.fetchone() 
+
+        if not record: 
+            return False 
+        
+        legit_hash = record[2].encode('utf8')
+        
+        print(legit_hash)
+        print(type(legit_hash))
+
+        return bcrypt.checkpw(password, legit_hash)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import psycopg2
+import bcrypt
 
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
+from auth import check_credentials
 
 conn = psycopg2.connect(database="dominik",
                         user="dominik",
@@ -21,24 +23,38 @@ class AuthData(BaseModel):
 @app.post("/register", status_code=201)
 async def register(data: AuthData, response: Response):
     username = data.username 
-    password = data.password 
 
     # Checking if login is already in use
     with conn.cursor() as cursor: 
         cursor.execute("SELECT * FROM users WHERE username=%s;", (username,))
         record = cursor.fetchone() 
 
-        print(record)
-
         if record: 
             response.status_code = status.HTTP_409_CONFLICT
             return 
 
+    password = data.password 
+    password = password.encode('utf-8') 
+    salt = bcrypt.gensalt() 
+
+    hash = bcrypt.hashpw(password, salt)
+
+    print(type(hash))
+    print(hash)
+
+    hash = hash.decode('utf8')
+    print("after", hash)
 
     with conn.cursor() as cursor:
-        cursor.execute("INSERT INTO users (username, password_hash) VALUES (%s, %s);", (username, password))
+        cursor.execute("INSERT INTO users (username, password_hash) VALUES (%s, %s);", (username, hash))
         conn.commit()
 
     
-
-
+@app.post("/login", status_code=200)
+async def login(data: AuthData, response: Response):
+    print("login")
+    if check_credentials(data.username, data.password, conn):
+        return 
+    else: 
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return 

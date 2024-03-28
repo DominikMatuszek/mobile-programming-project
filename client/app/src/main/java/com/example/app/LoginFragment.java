4abc81e96fd0eaa6feaffe7b0e9e387b37e6ccc8
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-03-28 15:40:33 +0100

    logging in works

--- client/app/src/main/java/com/example/app/LoginFragment.java
+++ client/app/src/main/java/com/example/app/LoginFragment.java
@@ -11,9 +11,36 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.app.databinding.FragmentLoginBinding;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+
+    private boolean loggedIn(String username, String password) {
+
+        try {
+            URL url = new URL("http://52.169.201.105:8000/login");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            String jsonInputString = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
+
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(jsonInputString.getBytes());
+
+            int responseCode = connection.getResponseCode();
+            return responseCode == 200;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 
     @Override
     public View onCreateView(
@@ -33,6 +60,25 @@ public class LoginFragment extends Fragment {
                     NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.action_LoginFragment_to_signupFragment);
                 }
         );
+
+        binding.button.setOnClickListener(v -> {
+            String username = binding.editTextText.getText().toString();
+            String password = binding.editTextTextPassword.getText().toString();
+
+            new Thread(() -> {
+                if (loggedIn(username, password)) {
+                    System.out.println("OK");
+                    getActivity().runOnUiThread(
+                            () -> NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.action_LoginFragment_to_signupFragment)
+                    );
+                } else {
+                    System.out.println("NOT OK");
+                }
+            }
+            ).start();
+
+
+        });
 
     }
 

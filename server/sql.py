ed9a90c967f984e82a72d785d945aa670035d527
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-04-16 22:08:15 +0200

    usernames are bad ids

--- server/sql.py
+++ server/sql.py
@@ -1,7 +1,21 @@
 import datetime 
 
+def get_user_id(username, conn):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT id FROM players WHERE username = %s;", (username,))
+        
+        result = cursor.fetchone()
+        
+        if result is None:
+            return None
+        
+        return result[0]
+
 def add_match_to_database(host, guest, targets, conn):
     timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    host_id = get_user_id(host, conn)
+    guest_id = get_user_id(guest, conn)
+    
     with conn.cursor() as cursor:
         cursor.execute("INSERT INTO matches (start_timestamp, end_timestamp) VALUES (%s, NULL);", (timestamp,))
         cursor.execute("SELECT max(id) FROM matches;") # I'm sorry. Haha.
@@ -14,8 +28,8 @@ def add_match_to_database(host, guest, targets, conn):
         for target in targets:
             cursor.execute("INSERT INTO targets (longtitude, latitude, match_id) VALUES (%s, %s, %s);", (target[0], target[1], match_id))
             
-        cursor.execute("INSERT INTO results (player_id, match_id, won) VALUES (%s, %s, NULL);", (host, match_id))
-        cursor.execute("INSERT INTO results (player_id, match_id, won) VALUES (%s, %s, NULL);", (guest, match_id))
+        cursor.execute("INSERT INTO results (player_id, match_id, won) VALUES (%s, %s, NULL);", (host_id, match_id))
+        cursor.execute("INSERT INTO results (player_id, match_id, won) VALUES (%s, %s, NULL);", (guest_id, match_id))
         
         conn.commit()
         

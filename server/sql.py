5d61cf56be9cefa8160441767e3e0d76db7bcdbc
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-04-16 22:04:28 +0200

    more shenanigans

--- server/sql.py
+++ server/sql.py
@@ -4,14 +4,10 @@ def add_match_to_database(host, guest, targets, conn):
     timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
     with conn.cursor() as cursor:
         cursor.execute("INSERT INTO matches (start_timestamp, end_timestamp) VALUES (%s, NULL);", (timestamp,))
-        cursor.execute("SELECT LASTVAL();")        
+        cursor.execute("SELECT max(id) FROM matches;") # I'm sorry. Haha.
         
-    
-        print(cursor.fetchone()[0])
-        
+        print("AAAAAAAAAAAAA", cursor.fetchone())
         match_id = cursor.fetchone()[0]
-        
-    
         
         for target in targets:
             cursor.execute("INSERT INTO targets (longtitude, latitude, match_id) VALUES (%s, %s, %s);", (target[0], target[1], match_id))

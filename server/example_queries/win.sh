1f9c6c0dfc9035bd42e0b09c0433a9553410bfaa
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-05-15 09:56:49 +0200

    attempts at making the history section scrollable

--- server/example_queries/win.sh
+++ server/example_queries/win.sh
@@ -5,8 +5,8 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "username": "xd",
-  "password": "xd",
+  "username": "xdd",
+  "password": "xdd",
   "lon": 19.94492696849495,
   "lat": 50.077469469404434
 }
@@ -18,8 +18,8 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "username": "xd",
-  "password": "xd",
+  "username": "xdd",
+  "password": "xdd",
   "lon": 19.933357948451192,
   "lat": 50.05297921835436
 }
@@ -31,8 +31,8 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "username": "xd",
-  "password": "xd",
+  "username": "xdd",
+  "password": "xdd",
   "lon": 19.808668596730975,
   "lat": 50.08102296159998
 }
